8628101e4ef06c6249c2f4264048b5285b6e116c
zayedibnibrahim <zayed.ibn@gmail.com>
2024-06-29 22:36:08 +0600

    resubmit

--- dist/app/modules/course/course.constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.courseFields = void 0;
-exports.courseFields = [
-    'title',
-    'instructor',
-    'details.description',
-    'provider',
-];
